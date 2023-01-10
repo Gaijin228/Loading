f1296dcf46904c65cd726db59b56f7c04ec095bb
Gaijin <capwelgaijin@gmail.com>
2023-01-10 10:59:13 +0000

    d'autres element ajoutes dans ProduitsTableSeeder

--- database/seeders/ProduitsTableSeeder.php
+++ database/seeders/ProduitsTableSeeder.php
@@ -23,5 +23,53 @@ class ProduitsTableSeeder extends Seeder
             'description' => Str::random(20),
             'photo_principale' => Str::random(10),
         ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 39,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 15,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 29,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 35,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 45,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 25,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 85,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
+        DB::table('produits')->insert([
+            'nom' => Str::random(10),
+            'prix_ht' => 65,
+            'description' => Str::random(20),
+            'photo_principale' => Str::random(10),
+        ]);
     }
 }
