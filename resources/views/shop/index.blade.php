a8d801e991de8dd374cab8bea92397cb9039041f
Gaijin <capwelgaijin@gmail.com>
2023-01-11 09:00:20 +0000

    index.blade rstructur avc 1e seule card en foreach

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -4,11 +4,12 @@
 @section('content')
     <div class="album py-5 bg-light">
         <div class="container">
-            @foreach ($produits as $produit)
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                @foreach ($produits as $produit)
                     <div class="col">
                         <div class="card shadow-sm">
-                            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="{{ asset('img/' . $produits->photo_principale ) }}" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></img>
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
                             <div class="card-body">
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -19,13 +20,12 @@
                                 </div>
                                 <small class="text-muted">9 mins</small>
                             </div>
+                            </div>
                         </div>
                     </div>
+                @endforeach
 
-
-
-                </div>
-            @endforeach
+            </div>
         </div>
     </div>
 @endsection
