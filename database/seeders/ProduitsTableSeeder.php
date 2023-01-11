d68412eca201415334837cfd26bfe8119ea5cb58
Gaijin <capwelgaijin@gmail.com>
2023-01-11 10:32:49 +0000

    migrate:fresh --seed --seeder=ProduitsTableSeeder

--- database/seeders/ProduitsTableSeeder.php
+++ database/seeders/ProduitsTableSeeder.php
@@ -20,55 +20,55 @@ class ProduitsTableSeeder extends Seeder
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 25,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '34'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 39,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '30'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 15,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '26'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 29,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '6'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 35,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '7'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 45,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '10'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 25,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '27'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 85,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => 't-shirt-eclair-bleu-printemps-de-bourges'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 65,
-            'description' => fake()->sentence(3, true),
+            'description' => fake()->paragraph(3, true),
             'photo_principale' => '35'.'.jpg',
         ]);
     }
