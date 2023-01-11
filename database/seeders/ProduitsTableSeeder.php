d51a95b31d52347e559742c626e98b3626bf67e6
Gaijin <capwelgaijin@gmail.com>
2023-01-11 11:04:48 +0000

    ProduitsTableSeeder modifier avec lorem20 index.blade ok

--- database/seeders/ProduitsTableSeeder.php
+++ database/seeders/ProduitsTableSeeder.php
@@ -20,55 +20,55 @@ class ProduitsTableSeeder extends Seeder
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 25,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nostrum ducimus voluptate officiis placeat autem iusto quia laudantium temporibus eligendi.',
             'photo_principale' => '34'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 39,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum rem ullam enim mollitia cumque? Aliquam quia tempora inventore excepturi delectus.',
             'photo_principale' => '30'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 15,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nemo harum saepe sint placeat. Perspiciatis, nulla. Dolorum nam velit quasi?',
             'photo_principale' => '26'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 29,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit earum distinctio aliquid quibusdam ducimus at pariatur numquam, eaque dolorem in.',
             'photo_principale' => '6'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 35,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum quae possimus quaerat voluptatem, dolore aspernatur iste odio velit enim.',
             'photo_principale' => '7'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 45,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id porro soluta doloremque dignissimos dolores sequi laudantium sapiente minus nemo.',
             'photo_principale' => '10'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 25,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ipsa recusandae ex eos consequatur quo fugiat optio eaque explicabo. Pariatur!',
             'photo_principale' => '27'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'male'),
             'prix_ht' => 85,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quisquam eos veritatis modi deserunt. Facere, necessitatibus recusandae? Ad, itaque facilis.',
             'photo_principale' => 't-shirt-eclair-bleu-printemps-de-bourges'.'.jpg',
         ]);
         DB::table('produits')->insert([
             'nom' => fake()->name($gender = 'female'),
             'prix_ht' => 65,
-            'description' => fake()->paragraph(3, true),
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, eligendi! Libero veniam, debitis cumque optio quasi necessitatibus illo accusantium dicta?',
             'photo_principale' => '35'.'.jpg',
         ]);
     }
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -13,7 +13,7 @@
                             <img class="bd-placeholder-img card-img-top" width="100%" height="300" src="{{ asset('img/' . $produit->photo_principale) }}" alt="mon produit">
 
                             <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text">{{ $produit->nom }} <br> {{ $produit->description }} </p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                 <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
