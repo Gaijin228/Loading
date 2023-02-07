2d1d27374ecc10de2ba6575dcad8bf4a264e18a4
Gaijin <capwelgaijin@gmail.com>
2023-02-07 16:16:15 +0000

    php artisan db:seed --class=CategoryTableSeeder

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,30 @@ class CategoryTableSeeder extends Seeder
     public function run()
     {
         //
+
+        $category = new Category();
+        $category->nom ="Films";
+        $category->is_online = 1;
+        $category->save();
+
+        $category2 = new Category();
+        $category2->nom ="Séries TV";
+        $category2->is_online = 1;
+        $category2->save();
+
+        $category3 = new Category();
+        $category3->nom ="Musique";
+        $category3->is_online = 1;
+        $category3->save();
+
+        $category4 = new Category();
+        $category4->nom ="Jeux-vidéos";
+        $category4->is_online = 1;
+        $category4->save();
+
+        $category5 = new Category();
+        $category5->nom ="Sport";
+        $category5->is_online = 1;
+        $category5->save();
     }
 }
