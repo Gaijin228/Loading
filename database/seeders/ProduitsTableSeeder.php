be8902335c939361769b546c82f3c4f41cf7c659
Gaijin <capwelgaijin@gmail.com>
2023-02-08 11:36:07 +0000

    modification de ProduitsTableSeeder

--- database/seeders/ProduitsTableSeeder.php
+++ database/seeders/ProduitsTableSeeder.php
@@ -18,58 +18,58 @@ class ProduitsTableSeeder extends Seeder
     public function run()
     {
         DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'male'),
+            'nom' => 'T-shirt Goonies',
             'prix_ht' => 25,
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nostrum ducimus voluptate officiis placeat autem iusto quia laudantium temporibus eligendi.',
-            'photo_principale' => '34'.'.jpg',
+            'description' => 'T-shirt du film les Goonies',
+            'photo_principale' => 'goonies'.'.jpg',
         ]);
         DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'male'),
+            'nom' => 'T-shirt Star Trek',
             'prix_ht' => 39,
-            'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum rem ullam enim mollitia cumque? Aliquam quia tempora inventore excepturi delectus.',
-            'photo_principale' => '30'.'.jpg',
+            'description' => 'T-shirt du film les Star Trek',
+            'photo_principale' => 'star_trek_kirk'.'.jpg',
         ]);
         DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'female'),
+            'nom' => 'T-shirt M. Happy',
             'prix_ht' => 15,
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nemo harum saepe sint placeat. Perspiciatis, nulla. Dolorum nam velit quasi?',
-            'photo_principale' => '26'.'.jpg',
+            'description' => 'T-shirt qui rend heureux',
+            'photo_principale' => 'happy'.'.jpg',
         ]);
         DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'female'),
+            'nom' => 'T-shirt Hulk',
             'prix_ht' => 29,
-            'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit earum distinctio aliquid quibusdam ducimus at pariatur numquam, eaque dolorem in.',
-            'photo_principale' => '6'.'.jpg',
+            'description' => 'T-shirt qui rend super fort',
+            'photo_principale' => 'hulk'.'.jpg',
         ]);
         DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'female'),
-            'prix_ht' => 35,
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum quae possimus quaerat voluptatem, dolore aspernatur iste odio velit enim.',
-            'photo_principale' => '7'.'.jpg',
-        ]);
-        DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'female'),
-            'prix_ht' => 45,
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id porro soluta doloremque dignissimos dolores sequi laudantium sapiente minus nemo.',
-            'photo_principale' => '10'.'.jpg',
-        ]);
-        DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'male'),
-            'prix_ht' => 25,
-            'description' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ipsa recusandae ex eos consequatur quo fugiat optio eaque explicabo. Pariatur!',
-            'photo_principale' => '27'.'.jpg',
-        ]);
-        DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'male'),
-            'prix_ht' => 85,
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quisquam eos veritatis modi deserunt. Facere, necessitatibus recusandae? Ad, itaque facilis.',
-            'photo_principale' => 't-shirt-eclair-bleu-printemps-de-bourges'.'.jpg',
-        ]);
-        DB::table('produits')->insert([
-            'nom' => fake()->name($gender = 'female'),
+            'nom' => 'T-shirt Krusty Le Clown',
             'prix_ht' => 65,
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, eligendi! Libero veniam, debitis cumque optio quasi necessitatibus illo accusantium dicta?',
-            'photo_principale' => '35'.'.jpg',
+            'description' => 'T-shirt qui rend super rigolo et cynique',
+            'photo_principale' => 'krusty_simpsons'.'.jpg',
+        ]);
+        DB::table('produits')->insert([
+            'nom' => 'T-shirt Little Miss Sunshine',
+            'prix_ht' => 35,
+            'description' => 'Une belle aventure familiale !',
+            'photo_principale' => 'little_miss_sunshine'.'.jpg',
+        ]);
+        DB::table('produits')->insert([
+            'nom' => 'T-shirt Les Simpsons',
+            'prix_ht' => 45,
+            'description' => 'T-shirt super cool',
+            'photo_principale' => 'simpsons'.'.jpg',
+        ]);
+        DB::table('produits')->insert([
+            'nom' => 'T-shirt SuperMan',
+            'prix_ht' => 25,
+            'description' => 'T-shirt Super man',
+            'photo_principale' => 'super_man'.'.jpg',
+        ]);
+        DB::table('produits')->insert([
+            'nom' => 'T-shirt Wonder Woman',
+            'prix_ht' => 85,
+            'description' => 'T-shirt Wonder Woman',
+            'photo_principale' => 'wonder_woman'.'.jpg',
         ]);
     }
 }
